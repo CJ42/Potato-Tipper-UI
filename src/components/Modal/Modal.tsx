97e851ec3261faef0cc19d3ad69b57d2581d6275
CJ42 <cavallerajean@gmail.com>
2025-09-23 21:47:10 +0100

    feat(wip): add modal to open when EOA wallet is opened

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,25 +4,31 @@ const Modal = ({
     isOpen,
     onClose,
     children,
+    closeDisable = false,
+    size = 3
 }: {
     title: string;
     isOpen: boolean;
     onClose: () => void;
     children: React.ReactNode;
+    closeDisable?: boolean;
+    size?: number;
 }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white rounded-lg p-6 max-w-3xl w-full mx-4 max-h-[80vh] overflow-y-auto">
+        <div className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm flex items-center justify-center z-50">
+            <div className={`bg-white rounded-lg p-6 max-w-${size == 0 ? '' : size}xl w-full mx-4 max-h-[80vh] overflow-y-auto`}>
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-2xl font-bold">{title}</h2>
-                    <button
-                        onClick={onClose}
-                        className="text-gray-500 hover:text-gray-700 text-2xl font-bold"
-                    >
-                        ×
-                    </button>
+                    {!closeDisable && (
+                        <button
+                            onClick={onClose}
+                            className="text-gray-500 hover:text-gray-700 text-2xl font-bold"
+                        >
+                            ×
+                        </button>
+                    )}
                 </div>
                 {children}
             </div>
